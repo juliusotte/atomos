258ca7bece4378ed379e7a0e02e418c546f94464
Julius Otte <otte@otte.cloud>
2022-07-04 21:38:32 +0200

    added domain model base

---
+++ source/atomos/domain/model/model.py
@@ -0,0 +1,13 @@
+import abc
+from typing import Optional, List
+from uuid import uuid4
+
+from atomos.domain.events import event
+
+
+class EventQueue(abc.ABC):
+    events: Optional[List[event.Event]] = []
+
+
+class Model(EventQueue):
+    id: Optional[str] = str(uuid4())
