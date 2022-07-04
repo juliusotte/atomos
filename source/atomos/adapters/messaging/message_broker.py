b3f024fe2b19ba24d5a5a74e08c56045710a7514
Julius Otte <otte@otte.cloud>
2022-07-04 21:34:41 +0200

    added messaging adapter

---
+++ source/atomos/adapters/messaging/message_broker.py
@@ -0,0 +1,23 @@
+import abc
+import logging
+from typing import Callable, Awaitable
+
+from atomos.domain.events import event
+
+logger = logging.getLogger(__name__)
+
+
+class MessageBroker(abc.ABC):
+    @abc.abstractmethod
+    async def publish(self, channel: str, event: event.Event):
+        logger.info('publishing: channel=%s, events=%s', channel, event)
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    async def subscribe(self, channel: str):
+        logger.info('subscribing: channel=%s', channel)
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    async def process(self, handle: Callable[..., Awaitable]):
+        raise NotImplementedError
---
+++ source/atomos/adapters/messaging/redis_pub_sub_broker.py
@@ -0,0 +1,24 @@
+import redis
+import json
+from dataclasses import asdict
+from typing import Callable, Awaitable
+
+from atomos import config
+from atomos.adapters.messaging import message_broker
+from atomos.domain.events import event
+
+
+class RedisPubSubBroker(message_broker.MessageBroker):
+    def __init__(self, host: str = config.REDIS_HOST, port: int = config.REDIS_PORT):
+        self._client = redis.Redis(host=host, port=port)
+        self._pub_sub = self._client.pubsub(ignore_subscribe_messages=True)
+
+    async def publish(self, channel: str, event: event.Event):
+        self._client.publish(channel, json.dumps(asdict(event)))
+
+    async def subscribe(self, channel: str):
+        self._pub_sub.subscribe(channel)
+
+    async def process(self, handle: Callable[..., Awaitable]):
+        for message in self._pub_sub.listen():
+            await handle(message)
